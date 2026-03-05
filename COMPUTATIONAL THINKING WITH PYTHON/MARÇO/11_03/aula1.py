83861cd212c1d9ba246e56b2d37118bd7dbedf1d
Felipe Cassiano <126339228+Felipe-Cassiano@users.noreply.github.com>
2026-03-05 19:57:52 -0300

    Initial commit
    12/03

---
+++ COMPUTATIONAL THINKING WITH PYTHON/MARÇO/11_03/aula1.py
@@ -0,0 +1,40 @@
+
+div = '---------------------------'
+
+#EXERCÍCIO 1
+meuNome = 'Felipe'
+print(f'Meu nome e {meuNome}')
+
+print(div)
+
+#EXERCÍCIO 2
+a,b = 3,5
+print(2*a+3*b)
+
+print(div)
+#EXERCÍCIO 3
+n1 = 1
+n2 = 2
+n3 = 3
+print(n1+n2+n3)
+
+print(div)
+
+#EXERCÍCIO 4
+salario = 1000
+print(salario+salario*.15)
+
+print(div)
+
+#EXERCÍCIO 5
+bonus = 1000
+print(bonus-bonus*.05)
+
+print(div)
+
+#EXERCÍCIO 6
+A,B = 10,20
+
+print(A,B)
+
+
