b41dad5a836d20567ae241d46c41e07362e2ecc9
Felipe Cassiano <felipe3.cassiano@gmail.com>
2026-03-17 19:15:11 -0300

    17/03

---
+++ EDGE COMPUTING E COMPUTER SYSTEMS/MARÇO/17_03/exercicioCruzamento.ino
@@ -0,0 +1,36 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(10, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+
+  digitalWrite(10, HIGH);
+  digitalWrite(5, HIGH);
+}
+
+void loop()
+{
+  delay(5000); // Wait for 5000 millisecond(s)
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(12, HIGH);
+  digitalWrite(10, LOW);
+  delay(5000); // Wait for 5000 millisecond(s)
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(11, LOW);
+  digitalWrite(10, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(7, LOW);
+  digitalWrite(5, HIGH);
+}
